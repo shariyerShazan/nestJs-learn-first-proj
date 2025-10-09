24357828c26dee5fff9ab25e48de08045665cfab
shariyerShazan <shariyershazan1@gmail.com>
2025-10-10 01:47:48 +0600

    get post put patch student

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,4 +1,44 @@
-import { Injectable } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 @Injectable()
-export class StudentService {}
+export class StudentService {
+    private students = [
+        {id: 1 , name: "Shazan" , age: 22},
+        {id: 2 , name: "Vijay" , age: 59},
+        {id: 3 , name: "Priya" , age: 23},
+    ];
+    // GET
+    getAllStudents(){
+        return this.students;
+    };
+    getStudentById(id: number){
+        const student = this.students.find((student) => student.id === id);
+        if(!student)throw new NotFoundException("Student not found!");
+        return student
+    }
+    // POST
+    createStudent(data: {name: string ; age: number}){
+        const newStudent = {
+            id: Date.now() ,
+            ...data ,
+        }
+        this.students.push(newStudent)
+        return newStudent ;
+    }
+    // PUT
+    updateStudent(id: number , data: {name: string ; age: number}){
+        const index = this.students.findIndex((student) => student.id === id);
+        if(index === -1)throw new NotFoundException("Student not found!")
+             this.students[index] = {id , ...data};
+             return this.students[index]
+     }
+
+    //  PATCH
+    patchStudent(id: number , data: Partial<{name: string ; age : number}>){
+        const student = this.getStudentById(id) 
+        Object.assign(student , data) ; // Object.assign change provided data from the prev data..
+        return student ;
+    }
+}
+
