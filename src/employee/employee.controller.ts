33f3b5990cfb1b2c0c3e18aa5b1e27e795015a0b
shariyerShazan <shariyershazan1@gmail.com>
2025-10-10 00:40:13 +0600

    exploye controller service and module done

--- src/employee/employee.controller.ts
+++ src/employee/employee.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { EmployeeService } from './employee.service';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('employee')
-export class EmployeeController {}
+export class EmployeeController {
+    constructor(private readonly employeeService : EmployeeService){}
+    @Get()
+    getEmployees(){
+         return this.employeeService.getEmployees();
+    }
+    @Get(":id")
+    getEmployeeById(@Param("id") id: string){
+        return this.employeeService.getEmployeeById(Number(id));
+    }
+}
--- src/employee/employee.service.ts
+++ src/employee/employee.service.ts
@@ -1,4 +1,19 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class EmployeeService {}
+export class EmployeeService {
+     private employees = [
+        {id: 1 , name : "Shazan"} ,
+        {id: 2 , name : "Vijay"} ,
+        {id: 3 , name : "Priya"} ,
+     ];
+     getEmployees(){
+        return this.employees ;
+     };
+     getEmployeeById(id: number){
+        return this.employees.find((employee)=>{
+            return employee.id === id
+        })
+     }
+}
