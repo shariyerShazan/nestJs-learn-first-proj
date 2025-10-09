3605babfd3e833898761f0acbce29c84f2fb87dd
shariyerShazan <shariyershazan1@gmail.com>
2025-10-10 03:14:56 +0600

    get post customer with data type and interface

--- src/customer/customer.controller.ts
+++ src/customer/customer.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { CustomerService } from './customer.service';
+/* eslint-disable prettier/prettier */
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateCustomerDto } from './dto/create-customer.dto';
 
 @Controller('customer')
-export class CustomerController {}
+export class CustomerController {
+    constructor(private readonly customerService : CustomerService){}
+
+    // get
+    @Get()
+    getAllCustomers(){
+        return this.customerService.getAllCustomers();
+    }
+
+    // post
+    @Post()
+    addCustomer(@Body() createCustomerDto : CreateCustomerDto){
+        return this.customerService.addCustomer(createCustomerDto);
+    }
+}
--- src/customer/customer.service.ts
+++ src/customer/customer.service.ts
@@ -1,3 +1,4 @@
+import { CreateCustomerDto } from './dto/create-customer.dto';
 /* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 import { Customer } from './interfaces/customer.interface';
@@ -5,4 +6,19 @@ import { Customer } from './interfaces/customer.interface';
 @Injectable()
 export class CustomerService {
     private customers : Customer[] = [];
+    // GET
+    getAllCustomers(): Customer[]{
+        return this.customers;
+    }
+
+    // POST
+    addCustomer(createCustomerDto: CreateCustomerDto): Customer {
+        const newCustomer : Customer = {
+            id: Date.now() ,
+            ...createCustomerDto,
+        }
+        this.customers.push(newCustomer)
+        return newCustomer ;
+    }
+    
 }
