e18063d254d0d5c22c13ab10490c61f98795e4da
shariyerShazan <shariyershazan1@gmail.com>
2025-10-10 03:40:30 +0600

    validation by class-validator

--- src/customer/dto/create-customer.dto.ts
+++ src/customer/dto/create-customer.dto.ts
@@ -1,5 +1,12 @@
 /* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+
+import { IsInt, IsString } from "class-validator";
+
+/* eslint-disable prettier/prettier */
 export class CreateCustomerDto {
+    @IsString()
     name: string ;
+    @IsInt()
     age : number ;
 }
